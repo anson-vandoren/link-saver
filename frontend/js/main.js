e12fe97cb145ce76e5adeb49995c85e9c7395df1
Anson VanDoren <anson@ansonvandoren.com>
2023-04-05 21:14:14 -0700

    fixed handlebars template

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,80 +1,124 @@
-$(document).ready(function () {
-  const apiUrl = 'http://localhost:3001';
-
-  function loadLinks() {
-    const token = localStorage.getItem('token');
-    if (!token) {
-      // If no token is found, redirect to the login page
-      window.location.href = 'login.html';
-      return;
-    }
-
-    // Fetch saved links from the backend API
-    $.ajax({
-      type: 'GET',
-      url: `${apiUrl}/api/links`,
-      headers: { Authorization: `Bearer ${token}` },
-      success: function (response) {
-        // Display the saved links
-        const linkList = $('#link-list');
-        linkList.empty();
-        response.links.forEach((link) => {
-          const listItem = `<li class="list-group-item">
-                              <h5>${link.title}</h5>
-                              <a href="${link.url}" target="_blank">${link.url}</a>
-                              <p>Tags: ${link.tags.join(', ')}</p>
-                              <p>Visibility: ${link.visibility}</p>
-                            </li>`;
-          linkList.append(listItem);
-        });
-      },
-      error: function (error) {
-        // Handle error (e.g., show an error message or redirect to the login page)
-        alert('Error fetching links. Please try again.');
-      },
-    });
-
-  }
-
-  // Load saved links when the page loads
-  loadLinks();
-
-  // Add-link form submission handler
-  $('#add-link-form').submit(function (e) {
-    e.preventDefault();
-    const token = localStorage.getItem('token');
-    const title = $('#link-title').val();
-    const url = $('#link-url').val();
-    const tags = $('#link-tags')
-      .val()
-      .split(',')
-      .map((tag) => tag.trim());
-    const visibility = $('#link-visibility').val();
-
-    // Send the new link to the backend API
-    $.ajax({
-      type: 'POST',
-      url: `${apiUrl}/api/links`,
-      headers: { Authorization: `Bearer ${token}` },
-      data: JSON.stringify({ title, url, tags, visibility }),
-      contentType: 'application/json',
-      success: function (response) {
-        // Add the new link to the list and clear the form inputs
-        loadLinks();
-        $('#link-title').val('');
-        $('#link-url').val('');
-        $('#link-tags').val('');
-      },
-      error: function (error) {
-        // Handle error (e.g., show an error message)
-        alert('Error adding link. Please try again.');
-      },
-    });
-  });
-
-  // Logout button click handler
-  $('#logout-btn').click(function () {
-    localStorage.removeItem('token');
-    window.location.href = 'login.html';
-  });
+document.addEventListener('DOMContentLoaded', () => {
+  init();
 });
+
+const API_URL = 'http://localhost:3001';
+
+async function init() {
+  if (localStorage.getItem('token')) {
+    document.getElementById('logout-btn').addEventListener('click', handleLogoutButtonClick);
+    document.getElementById('add-link-form').addEventListener('submit', handleAddLinkFormSubmit);
+
+    loadLinks();
+  } else {
+    window.location.href = '/login.html';
+  }
+}
+
+async function getLinks() {
+  const response = await fetch(`${API_URL}/api/links`, {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${localStorage.getItem('token')}`
+    }
+  });
+
+  if (response.ok) {
+    return await response.json();
+  } else {
+    throw new Error('Failed to load links');
+  }
+}
+
+async function loadLinks() {
+  const links = await getLinks();
+
+  // Get the Handlebars template and compile it
+  const templateSource = document.getElementById('links-list').innerHTML;
+  const template = Handlebars.compile(templateSource);
+
+  // Expand the template with the links data
+  const linksElement = document.getElementById('link-list');
+  linksElement.innerHTML = template(links);
+  console.log(links)
+
+  // Add event listeners for edit and delete buttons
+  const editButtons = document.querySelectorAll('.edit-link');
+  const deleteButtons = document.querySelectorAll('.delete-link');
+
+  editButtons.forEach((button) => {
+    button.addEventListener('click', handleEditButtonClick);
+  });
+
+  deleteButtons.forEach((button) => {
+    button.addEventListener('click', handleDeleteButtonClick);
+  });
+}
+
+async function handleLogoutButtonClick() {
+  localStorage.removeItem('token');
+  window.location.href = '/login.html';
+}
+
+async function handleAddLinkFormSubmit(event) {
+  event.preventDefault();
+
+  const title = document.getElementById('link-title').value;
+  const url = document.getElementById('link-url').value;
+  const tags = document.getElementById('link-tags').value.split(',').map(tag => tag.trim());
+  const visibility = document.getElementById('link-visibility').value;
+
+  try {
+    await addLink({ title, url, tags, visibility });
+    document.getElementById('add-link-form').reset();
+    loadLinks();
+  } catch (error) {
+    console.error('Error adding link:', error);
+  }
+}
+
+async function addLink(linkData) {
+  const response = await fetch(`${API_URL}/api/links`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${localStorage.getItem('token')}`
+    },
+    body: JSON.stringify(linkData)
+  });
+
+  if (!response.ok) {
+    throw new Error('Failed to add link');
+  }
+}
+
+async function handleEditButtonClick(event) {
+  const linkId = event.target.dataset.id;
+  // Load the link and show the edit form
+  // TODO: Implement the edit functionality
+  console.log('Edit link with id:', linkId);
+}
+
+async function handleDeleteButtonClick(event) {
+  const linkId = event.target.dataset.id;
+  try {
+    await deleteLink(linkId);
+    loadLinks();
+  } catch (error) {
+    console.error('Error deleting link:', error);
+  }
+}
+
+async function deleteLink(id) {
+  const response = await fetch(`${API_URL}/api/links/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error('Failed to delete link');
+  }
+}
