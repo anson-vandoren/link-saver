d91f89d2dae2bb5409dc5cce5a7938edece8dea4
Anson VanDoren <anson@ansonvandoren.com>
2023-04-14 06:34:23 -0700

    fixed break on entry

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const DB_PATH = process.env.DB_PATH || './database.sqlite3';
+const DB_PATH = process.env.DB_PATH || './dev.sqlite3';
 
 const sequelize = new Sequelize({
   dialect: 'sqlite',
