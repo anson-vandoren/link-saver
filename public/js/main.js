0e19c884f1d8237ff3357756d730cfb1750f151a
Anson VanDoren <anson@ansonvandoren.com>
2023-04-17 20:47:04 -0700

    fixes #14

--- public/js/main.js
+++ public/js/main.js
@@ -97,28 +97,36 @@ function divWithClasses(classes) {
 }
 
 function timeAgo(date) {
+  const pluralizeAndConcat = (n, word) => {
+    if (n > 1) word = `${word}s`;
+    return `${n} ${word} ago`;
+  }
+  
+
   const seconds = Math.floor((new Date() - date) / 1000);
   let interval = Math.floor(seconds / 31536000);
   if (interval >= 1) {
-    return interval + " years ago";
+    return pluralizeAndConcat(interval, 'year');
   }
+  
   interval = Math.floor(seconds / 2592000);
   if (interval >= 1) {
-    return interval + " months ago";
+    return pluralizeAndConcat(interval, 'month');
   }
   interval = Math.floor(seconds / 86400);
   if (interval >= 1) {
-    return interval + " days ago";
+    return pluralizeAndConcat(interval, 'day');
   }
   interval = Math.floor(seconds / 3600);
   if (interval >= 1) {
-    return interval + " hours ago";
+    return pluralizeAndConcat(interval, 'hour');
   }
   interval = Math.floor(seconds / 60);
   if (interval >= 1) {
-    return interval + " minutes ago";
+    return pluralizeAndConcat(interval, 'minute');
   }
-  return Math.floor(seconds) + " seconds ago";
+  interval = Math.floor(seconds);
+  return pluralizeAndConcat(interval, 'second');
 }
 
 /**
