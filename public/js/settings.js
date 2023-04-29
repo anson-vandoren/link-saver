22557ad97d36a688d1b3fd9c67ea444b8a5dbbb0
Anson VanDoren <anson@ansonvandoren.com>
2023-04-29 16:32:55 -0700

    fix #18 - add theme switching

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,8 +1,4 @@
-import {
-  API_URL,
-  showNotification,
-  handleLogoutButtonClick,
-} from './common.js';
+import { API_URL, showNotification, handleLogoutButtonClick } from './common.js';
 import { wsHandler } from './ws.js';
 
 const fileInput = document.querySelector('#import-bookmarks-file input[type=file]');
@@ -83,6 +79,45 @@ async function exportBookmarks() {
   }
 }
 
+async function fetchThemes() {
+  const response = await fetch('https://jenil.github.io/bulmaswatch/api/themes.json');
+  const data = await response.json();
+  return data.themes;
+}
+
+async function populateThemeDropdown() {
+  const themes = await fetchThemes();
+  const themeSelector = document.getElementById('theme-selector');
+
+  // Retrieve the current theme from local storage or use the default theme
+  const currentTheme = localStorage.getItem('selectedTheme') || 'https://unpkg.com/bulmaswatch@0.8.1/darkly/bulmaswatch.min.css';
+
+  themes.forEach((theme) => {
+    const option = document.createElement('option');
+    option.value = theme.css;
+    option.textContent = theme.name;
+
+    // If the theme matches the current theme, set the option as selected
+    if (theme.css === currentTheme) {
+      option.selected = true;
+    }
+
+    themeSelector.appendChild(option);
+  });
+}
+
+
+function saveThemeHandler() {
+  const themeSelector = document.getElementById('theme-selector');
+
+  themeSelector.addEventListener('change', (event) => {
+    const selectedTheme = event.target.value;
+    const stylesheetLink = document.getElementById('theme-stylesheet');
+    stylesheetLink.href = selectedTheme;
+    localStorage.setItem('selectedTheme', selectedTheme);
+  });
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   fileInput.onchange = () => {
     if (fileInput.files.length > 0) {
@@ -96,4 +131,6 @@ document.addEventListener('DOMContentLoaded', () => {
   exportBtn.addEventListener('click', () => {
     exportBookmarks();
   });
+  populateThemeDropdown();
+  saveThemeHandler();
 });
