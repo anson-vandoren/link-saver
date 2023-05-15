5ab0df2670c869ac54a35c394d4139e230cc5c39
Anson VanDoren <anson@ansonvandoren.com>
2023-05-15 08:18:58 -0700

    WIP #58

--- public/js/tagsBar.ts
+++ public/js/tagsBar.ts
@@ -33,7 +33,7 @@ function generateTagsHtml(tags: string[], onClick: () => void, group = true) {
   for (const char of Object.keys(groupedTags)) {
     const block = document.createElement('div');
     block.classList.add('block');
-
+    // TODO: use Bulma's tag class
     groupedTags[char].forEach((tag: string, index: number) => {
       const isFirst = index === 0;
       const tagLink = createTagLink(tag.toLowerCase(), {
@@ -54,6 +54,33 @@ function generateTagsHtml(tags: string[], onClick: () => void, group = true) {
   return tagsFragment;
 }
 
+function addActiveTagsHtml(activeTags: string[]) {
+  const activeTagsDiv = getElementById('active-tags-list', HTMLDivElement);
+  if (!activeTags.length) {
+    activeTagsDiv.innerHTML = '<hr />';
+    activeTagsDiv.classList.add('is-hidden');
+    return;
+  }
+  // add a <p> child and then add the tags to it as static text
+  const activeTagsParagraph = document.createElement('p');
+  activeTagsParagraph.classList.add('tags', 'are-small');
+  activeTags.forEach((tag, index) => {
+    // TODO: clicking a tag should remove it from the active tags
+    const tagSpan = document.createElement('span');
+    tagSpan.classList.add('tag', 'is-info');
+    tagSpan.textContent = tag;
+    if (index > 0) {
+      tagSpan.style.marginLeft = '8px';
+    }
+    activeTagsParagraph.appendChild(tagSpan);
+  });
+  // activeTagsDiv should have the tags followed by <hr />
+  activeTagsDiv.innerHTML = '';
+  activeTagsDiv.appendChild(activeTagsParagraph);
+  activeTagsDiv.appendChild(document.createElement('hr'));
+  activeTagsDiv.classList.remove('is-hidden');
+}
+
 async function fetchAndRenderTags(onClick: () => void, order: 'name' | 'links') {
   const tagsList = document.getElementById('tagsList');
   if (!tagsList) return;
@@ -61,8 +88,13 @@ async function fetchAndRenderTags(onClick: () => void, order: 'name' | 'links') 
   const searchTerm = searchInput instanceof HTMLInputElement ? searchInput.value : '';
   const tags = await getTags(order, searchTerm);
 
+  const tagsToSkip = searchTerm.split(' ')
+    .filter((term) => term.startsWith('#'))
+    .map((term) => term.slice(1));
+  addActiveTagsHtml(tagsToSkip);
+  const tagsToRenderLinks = tags.filter((tag) => !tagsToSkip.includes(tag));
   const group = order === 'name';
-  const tagsContainer = generateTagsHtml(tags, onClick, group);
+  const tagsContainer = generateTagsHtml(tagsToRenderLinks, onClick, group);
   tagsList.innerHTML = '';
   tagsList.appendChild(tagsContainer);
 }
